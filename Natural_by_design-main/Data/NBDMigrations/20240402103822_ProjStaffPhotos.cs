9ad42e63333cb63b7e3454a6494daa530327afcd
ChikawaS04 <ddavidson644@gmail.com>
2024-04-02 16:07:27 -0400

    first commit

---
+++ Natural_by_design-main/Data/NBDMigrations/20240402103822_ProjStaffPhotos.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBD3.Data.NBDMigrations
+{
+    /// <inheritdoc />
+    public partial class ProjStaffPhotos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BidStatus",
+                table: "Bids",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BidStatusList",
+                table: "Bids",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ProjectPhotos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    MimeType = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    ProjectID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectPhotos", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ProjectPhotos_Projects_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectThumbnails",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    MimeType = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    ProjectID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectThumbnails", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ProjectThumbnails_Projects_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StaffPhotos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    MimeType = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    StaffID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffPhotos", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_StaffPhotos_Staffs_StaffID",
+                        column: x => x.StaffID,
+                        principalTable: "Staffs",
+                        principalColumn: "StaffID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StaffThumbnails",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    MimeType = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    StaffID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffThumbnails", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_StaffThumbnails_Staffs_StaffID",
+                        column: x => x.StaffID,
+                        principalTable: "Staffs",
+                        principalColumn: "StaffID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectPhotos_ProjectID",
+                table: "ProjectPhotos",
+                column: "ProjectID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectThumbnails_ProjectID",
+                table: "ProjectThumbnails",
+                column: "ProjectID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffPhotos_StaffID",
+                table: "StaffPhotos",
+                column: "StaffID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffThumbnails_StaffID",
+                table: "StaffThumbnails",
+                column: "StaffID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectPhotos");
+
+            migrationBuilder.DropTable(
+                name: "ProjectThumbnails");
+
+            migrationBuilder.DropTable(
+                name: "StaffPhotos");
+
+            migrationBuilder.DropTable(
+                name: "StaffThumbnails");
+
+            migrationBuilder.DropColumn(
+                name: "BidStatus",
+                table: "Bids");
+
+            migrationBuilder.DropColumn(
+                name: "BidStatusList",
+                table: "Bids");
+        }
+    }
+}
---
+++ Natural_by_design-main/Models/Bid.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NBD3.Models
+{
+	public enum BidStatusList
+	{
+		NeedsRevision,
+		RejectedByClient,
+		RejectedByNBD,
+		Rejected,
+		ApprovedByClient,
+		ApprovedByNBD,
+		Approved
+	}
+
+
+	public class Bid
+	{
+		public int BidId { get; set; }
+
+		[Required(ErrorMessage = "You must enter a Bid amount.")]
+		[Display(Name = "Bid Extra fee")]
+		[DataType(DataType.Currency)]
+		public double BidAmount { get; set; }
+
+		[Required(ErrorMessage = "You must enter a date for the Bid.")]
+		[Display(Name = "Date")]
+		[DataType(DataType.Date)]
+		public DateOnly BidDate { get; set; }
+
+		[Display(Name = "Bid Cost")]
+		[DataType(DataType.Currency)]
+		public double BidCost { get; set; }
+
+		[Display(Name = "Bid Approve")]
+		public bool BidApprove { get; set; }
+
+		[Display(Name = "Status")]
+		public string BidStatus { get; set; } = "";
+
+		[Required(ErrorMessage = "You must select a Bid Status!")]
+		public BidStatusList BidStatusList { get; set; }
+
+
+		[Display(Name = "Project Name")]
+		[Required(ErrorMessage = "You must choose a project for this Bid.")]
+		public int ProjectID { get; set; }
+		public Project Project { get; set; }
+
+		public ICollection<Track> Tracks { get; set; } = new HashSet<Track>();
+
+		//Staff many to many
+		public ICollection<StaffDetail> StaffDetails { get; set; } = new HashSet<StaffDetail>();
+
+
+		//Labour many to many
+		public ICollection<LabourDetail> LabourDetails { get; set; } = new HashSet<LabourDetail>();
+
+		//Material many to many
+		public ICollection<MaterialDetail> MaterialDetails { get; set; } = new HashSet<MaterialDetail>();
+
+
+
+		//Prototype 2 adding materials, labours and staff
+		public Bid()
+		{
+			StaffDetails = new List<StaffDetail>();
+			LabourDetails = new List<LabourDetail>();
+		}
+
+		public void AddMaterial(Inventory inventory, int quantity)
+		{
+			MaterialDetails.Add(new MaterialDetail { Inventory = inventory, Quantity = quantity });
+		}
+
+		public void AddLabour(Labour labour, int quantity)
+		{
+			LabourDetails.Add(new LabourDetail { Labour = labour, Quantity = quantity });
+		}
+
+		public void AddStaff(Staff staff)
+		{
+			StaffDetails.Add(new StaffDetail { Staff = staff });
+		}
+
+	}
+
+
+}
+
+
